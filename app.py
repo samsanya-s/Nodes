db51aff08a6a1dc41d56335aa0674b7284067cd1
samsanya-s <medvedevsassha@yandex.ru>
2024-11-22 17:54:58 +0700

    Big Upddate

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, session, redirect, url_for
+from flask import Flask, request, jsonify, session, redirect, url_for, render_template
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import csv
@@ -9,7 +9,8 @@ app.secret_key = 'your_secret_key'  # Замените на более безо�
 
 # Путь к базе данных
 DATABASE = 'users.db'
-CSV_FILE = 'click_speed_results.csv'
+app.config['SESSION_COOKIE_SAMESITE'] = None
+app.config['SESSION_COOKIE_SECURE'] = False
 
 
 # Инициализация базы данных
@@ -21,27 +22,48 @@ def init_db():
                           username TEXT UNIQUE NOT NULL,
                           password TEXT NOT NULL
                       )''')
+        cursor.execute('''
+               CREATE TABLE IF NOT EXISTS user_node_data (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   user_id TEXT NOT NULL,
+                   node_name TEXT NOT NULL,
+                   timestamp INTEGER NOT NULL
+               )
+           ''')
         conn.commit()
+        print(conn)
 
 
 init_db()
 
-# Проверяем, существует ли CSV файл; если нет, создаем с заголовком
-if not os.path.exists(CSV_FILE):
-    with open(CSV_FILE, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['username', 'time_elapsed'])  # Заголовок
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        is_register = 'is_register' in request.form  # Проверка чекбокса
+
+        if is_register:
+            answer = register(username, password)
+            if answer["status"] == "success":
+                return redirect('/')
+            else:
+                error = answer["message"]
+        else:
+            answer = login(username, password)
+            if answer["status"] == "success":
+                return redirect('/')
+            else:
+                error = answer["message"]
+    return render_template('authorization.html', error=error)
 
 
-# Маршрут для регистрации пользователя
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-
-    if not username or not password:
-        return jsonify({'status': 'error', 'message': 'Username and password are required'}), 400
+def register(username, password):
+    if not username:
+        return {'status': 'error', 'message': 'Username are required'}
+    if not password:
+        return {'status': 'error', 'message': 'Password are required'}
 
     hashed_password = generate_password_hash(password)
     try:
@@ -49,28 +71,28 @@ def register():
             cursor = conn.cursor()
             cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
             conn.commit()
-        return jsonify({'status': 'success', 'message': 'User registered successfully'})
+            login(username, password)
+        return {'status': 'success', 'message': 'User registered successfully'}
     except sqlite3.IntegrityError:
-        return jsonify({'status': 'error', 'message': 'Username already taken'}), 400
+        return {'status': 'error', 'message': 'Username already taken'}
 
 
-# Маршрут для входа пользователя
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-
-    with sqlite3.connect(DATABASE) as conn:
+def login(username, password):
+    with (sqlite3.connect(DATABASE) as conn):
         cursor = conn.cursor()
-        cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
-        user = cursor.fetchone()
+        cursor.execute("SELECT password, id FROM users WHERE username = ?", (username,))
+        user_password, user_id = cursor.fetchone()
+        # print(user_password, cursor.fetchone())
 
-        if user and check_password_hash(user[0], password):
-            session['username'] = username
-            return jsonify({'status': 'success'})
+        if user_password:
+
+            if check_password_hash(user_password, password):
+                session['username'] = user_id
+                return {'status': 'success', 'message': "User logged in successfully"}
+            else:
+                return {'status': 'error', 'message': 'Invalid password'}
         else:
-            return jsonify({'status': 'error', 'message': 'Invalid username or password'}), 400
+            return {'status': 'error', 'message': 'Invalid username'}
 
 
 # Маршрут для сохранения данных времени
@@ -79,29 +101,61 @@ def save_time():
     if 'username' not in session:
         return jsonify({'status': 'error', 'message': 'User not logged in'}), 403
 
+    if not request.is_json:
+        return jsonify({"error": "Invalid JSON format"}), 401
+
     data = request.get_json()
-    time_elapsed = data.get('timeElapsed')
-    username = session['username']
+    node_name = data.get('node')
+    timestamp = data.get('time')
 
-    # Записываем время и имя пользователя в CSV файл
-    with open(CSV_FILE, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([username, time_elapsed])
+    # Проверяем наличие всех необходимых данных
+    if not node_name or not timestamp:
+        return jsonify({"error": "Missing 'node_name' or 'time' in request"}), 402
 
-    return jsonify({'status': 'success', 'time_elapsed': time_elapsed})
+    try:
+        # Преобразуем время в стандартный формат
+        mn = [600, 60, 1]
+        timestamp = sum([int(el) * mn[i] for i, el in enumerate(timestamp.split(":")[1:])])
+    except ValueError:
+        return jsonify({"error": "Invalid time format"}), 400
+
+    # Сохраняем данные в базу данных
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('''
+                DELETE FROM user_node_data
+                WHERE id IN (
+                    SELECT id FROM user_node_data
+                    WHERE user_id = ? AND node_name = ?
+                    ORDER BY id ASC
+                    LIMIT (SELECT CASE WHEN COUNT(*) > 100 THEN COUNT(*) - 100 ELSE 0 END
+                           FROM user_node_data
+                           WHERE user_id = ? AND node_name = ?)
+                )
+            ''', (session["username"], node_name, session["username"], node_name))
+    cursor.execute('''
+            INSERT INTO user_node_data (user_id, node_name, timestamp)
+            VALUES (?, ?, ?)
+        ''', (session["username"], node_name, timestamp))
+    # conn.commit()
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Data saved successfully"}), 200
 
 
 # Маршрут для выхода
-@app.route('/logout', methods=['POST'])
+@app.route('/logout', methods=['GET'])
 def logout():
     session.pop('username', None)
-    return jsonify({'status': 'success', 'message': 'Logged out successfully'})
+    return redirect('/')
 
-
-# Маршрут для отображения страницы
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+@app.route('/', methods=['GET'])
+def home():
+    if 'username' in session:
+        return render_template('main.html')
+    return redirect(url_for('auth'))
 
 
 if __name__ == '__main__':
---
+++ static/nodesScript.js
@@ -0,0 +1,172 @@
+let counter = 3;
+const overlay = document.getElementById('overlay');
+mainContent = document.getElementById('mainContent');
+const countdownElement = document.getElementById('countdown');
+const startText = document.getElementById('startText');
+const circleContainer = document.getElementById('circleContainer');
+const circle = document.querySelector('.progress-ring__circle');
+const radius = circle.r.baseVal.value;
+const circumference = 2 * Math.PI * radius;
+setOverlay();
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
+
+let nodes = [];
+let last_nodes = [];
+const count_last_nodes = 5;
+let currentNode = null;
+let imageShown = false;
+let timerInterval = null;
+let milisecondEplaced = 0; // Переменная для хранения времени
+let secondEplaced = 0;
+let minuteEplaced = 0;
+
+// Загрузка данных узлов из JSON файла
+fetch('static\\nodes.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Сеть ответила с ошибкой ' + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        nodes = data;
+
+    })
+    .catch(error => {
+        console.error('Ошибка загрузки JSON:', error);
+    });
+
+function setOverlay(){
+    counter = 3
+    mainContent.classList.add('hidden');
+    overlay.classList.add('d-flex');
+    document.body.addEventListener('click', startCountdown, { once: true });
+}
+
+function setProgress(percent) {
+    const offset = circumference - (percent / 100) * circumference;
+    circle.style.strokeDashoffset = offset;
+}
+
+function startCountdown() {
+
+    startText.classList.add('hidden');
+    circleContainer.classList.remove('hidden');
+    circleContainer.classList.add('circle-container');
+    countdownElement.textContent = counter;
+    setProgress(0); // Начинаем с полного заполнения
+
+    const intervalDuration = 500;
+    const countPerCircle = 100;
+    var currentCount = 0;
+    const countdownInterval = setInterval(() => {
+        currentCount += intervalDuration / countPerCircle;
+        if (currentCount > intervalDuration){
+            counter--;
+            currentCount -= intervalDuration;
+        }
+        if (counter > 0) {
+            countdownElement.textContent = counter > 0 ? counter : '';
+//            console.log(currentCount / intervalDuration * 100);
+            setProgress(currentCount / intervalDuration * 100);
+        } else {
+            startText.classList.remove('hidden');
+            circleContainer.classList.remove('circle-container');
+            circleContainer.classList.add('hidden');
+            overlay.classList.remove('d-flex');
+            mainContent.classList.remove('hidden');
+            getRandomNode()
+//                    overlay.style.display = 'none';
+
+            clearInterval(countdownInterval);
+        }
+    }, intervalDuration / countPerCircle);
+}
+
+function getRandomNode() {
+    let randomIndex = Math.floor(Math.random() * nodes.length);
+    while (randomIndex in last_nodes) {
+        randomIndex = Math.floor(Math.random() * nodes.length);
+    }
+
+    last_nodes.push(randomIndex);
+    if (last_nodes.length > count_last_nodes) {
+        last_nodes.shift();
+    }
+//    const randomIndex = 1;
+
+    currentNode = nodes[randomIndex];
+    document.getElementById('nodeName').textContent = currentNode.name;
+    document.getElementById('nodeImage').style.display = 'none';
+    document.getElementById('toggleButton').textContent = 'Показать узел';
+    imageShown = false;
+
+    // Сброс таймера при новом узле
+    resetTimer();
+    startTimer();
+
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    milisecondEplaced = 0; // Сброс времени
+    secondEplaced = 0;
+    minuteEplaced = 0;
+    document.getElementById('timer').textContent = 'Время: 00:00:00'; // Обновление отображения
+}
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        milisecondEplaced++;
+        if (milisecondEplaced >= 100){
+            milisecondEplaced -= 100;
+            secondEplaced++;
+        }
+        if (secondEplaced >= 60){
+            secondEplaced -= 60;
+            minuteEplaced++;
+        }
+        document.getElementById('timer').textContent = 'Время: ' + addLeadingNumberZeros(minuteEplaced, 2) + ":" + addLeadingNumberZeros(secondEplaced, 2) + ":" + addLeadingNumberZeros(milisecondEplaced, 2);
+    }, 10); // Обновление каждую секунду
+}
+
+
+function addLeadingNumberZeros(number, totalLength) {
+  return String(number).padStart(totalLength, '0');
+}
+function logout() {
+    resetTimer();
+    fetch('/logout', { method: 'GET' });
+//    .catch(error => console.error("Logout error:", error));
+}
+
+function save_time(){
+    fetch('/save_time', { method: 'POST',
+                          headers: { 'Content-Type': 'application/json' },
+                          body: JSON.stringify({ node: document.getElementById('nodeName').textContent, time: document.getElementById('timer').textContent })
+     }).catch(error => console.error("error:", error));
+    setOverlay();
+}
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const nodeImage = document.getElementById('nodeImage');
+
+    if (!imageShown) {
+        // Показать изображение узла
+        nodeImage.src = currentNode.image;
+        nodeImage.style.display = 'block';
+        this.textContent = 'Показать инструкцию';
+    } else {
+        // Показать изображение с инструкцией
+        nodeImage.src = currentNode.instruction;
+        this.textContent = 'Показать узел';
+    }
+
+    imageShown = !imageShown;
+});
+
+document.getElementById('cancel').addEventListener('click', setOverlay);
+document.getElementById('success').addEventListener('click', save_time);
+document.body.addEventListener('click', startCountdown, { once: true });
